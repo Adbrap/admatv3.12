b4d16c7ccad99a69abbff246633aa00c6e0e1992
Adbrap <adprog33@gmail.com>
2022-12-22 09:40:24 +0100

    first commit

---
+++ launcher.sh
@@ -0,0 +1,417 @@
+clear
+
+echo "                                                                         "
+echo "                                                                         "
+echo "                                                                         "
+echo "                                     ▄▄▄      ▓█████▄  ███▄ ▄███▓ ▄▄▄     ▄▄▄█████▓"
+echo "                                    ▒████▄    ▒██▀ ██▌▓██▒▀█▀ ██▒▒████▄   ▓  ██▒ ▓▒"
+echo "                                    ▒██  ▀█▄  ░██   █▌▓██    ▓██░▒██  ▀█▄ ▒ ▓██░ ▒░"
+echo "                                    ░██▄▄▄▄██ ░▓█▄   ▌▒██    ▒██ ░██▄▄▄▄██░ ▓██▓ ░ "
+echo "                                     ▓█   ▓██▒░▒████▓ ▒██▒   ░██▒ ▓█   ▓██▒ ▒██▒ ░ "
+echo "                                     ▒▒   ▓▒█░ ▒▒▓  ▒ ░ ▒░   ░  ░ ▒▒   ▓▒█░ ▒ ░░ "
+echo "                                     ▒   ▒▒ ░ ░ ▒  ▒ ░  ░      ░  ▒   ▒▒ ░   ░  "
+echo "                                    ░   ▒    ░ ░  ░ ░      ░     ░   ▒    ░   "
+echo "                                        ░  ░   ░           ░         ░  ░   "
+echo "                                             ░     "
+
+
+
+
+
+python3 /home/ubuntu/Desktop/noel2/BTC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ETH.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BNB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/XRP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ADA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SOL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/DOGE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/MATIC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/DOT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/DAI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SHIB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/TRX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/AVAX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/UNI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/WBTC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/LEO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/LTC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ATOM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/LINK.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ETC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/XLM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/FTT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/NEAR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CRO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/XMR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ALGO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BCH.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/LUNC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/QNT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/FLOW.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/VET.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/TON.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/APE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/FIL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ICP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/HBAR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/EGLD.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/XTZ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/MANA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SAND.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CHZ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/EOS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/AAVE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/THETA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/AXS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/USDP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/OKB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BSV.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/TUSD.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/MKR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/KCS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ZEC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/USDD.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/HT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/XEC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/MIOTA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BTT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/USDN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CAKE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/GRT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/NEO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/HNT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/KLAY.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/FTM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/PAXG.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SNX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/RUNE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/NEXO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/GT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/DASH.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CRV.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BAT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ENJ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/LDO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/STX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/KAVA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/COMP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ZIL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/FEI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/RVN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/MINA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/WAVES.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/XDC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/USDTC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/GMT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/TWT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/LRC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/DCR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CVX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CSPR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/XEM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/KSM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CELO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BTG.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ENS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/1INCH.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/HOT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/LUNA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/RSR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BNX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/GUSD.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ROSE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/YFI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/QTUM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/AR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ANKR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/GNO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/GALA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/TFUEL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/KDA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/IOTX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/GLM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BTRST.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/JST.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CEL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/POLY.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ONE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/HIVE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/OMG.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/LPT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ZRX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BAL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/FLUX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/IOST.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ICX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BORA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/AMP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/XYM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SRM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/KNC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ONT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/OP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/GLMR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CHSB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/STORJ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/WAXP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/WOO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ZEN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/XCH.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SXP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SUSHI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/IMX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/MXC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/AUDIO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/NFT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/UMA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/PLA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SKL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ABBC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SCRT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SLP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/DGB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ELON.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/INJ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/VGX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CKB.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CVC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/LSK.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/VLX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/PUNDIX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/COTI.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/KEEP.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/DAO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/REN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/MED.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/RNDR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CEEK.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/REEF.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/EWT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/PEOPLE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/BTCST.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/WIN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/NU.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CELR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SYS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ONG.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/XNO.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/REQ.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/DAG.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ORBS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SPELL.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/RLC.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/OCEAN.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/API3.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/SNT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/TRIBE.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ARDR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/PYR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/FX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/UOS.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/POWER.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/ACA.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/PROM.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/DENT.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/CHR.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/MX.py &
+sleep 1
+python3 /home/ubuntu/Desktop/noel2/%launcher%
+
+
